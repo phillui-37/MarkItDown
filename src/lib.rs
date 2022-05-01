0220c56a9d1bc501e51910ac5edc770dee7dad1b
Phil Lui <phillui37@gmail.com>
2022-05-01 21:19:51 +0800

    fixed issue bring by egui/eframe 0.18.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-#![forbid(unsafe_code)]
-#![cfg_attr(not(debug_assertions), deny(warnings))]
 #![warn(clippy::all, rust_2018_idioms)]
 
 mod app;
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-#![forbid(unsafe_code)]
-#![cfg_attr(not(debug_assertions), deny(warnings))] // Forbid warnings in release builds
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] //Hide console window in release builds on Windows, this blocks stdout.
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    let app = mark_it_down::TemplateApp::default();
-    let native_options = eframe::NativeOptions::default();
-    eframe::run_native(Box::new(app), native_options);
+    // Log to stdout (if you run with `RUST_LOG=debug`).
+    tracing_subscriber::fmt::init();
+    let native_options = eframe::NativeOptions {
+        transparent: true,
+        drag_and_drop_support: true,
+        ..Default::default()
+    };
+    eframe::run_native(
+        "eframe template",
+        native_options,
+        Box::new(|cc| Box::new(mark_it_down::TemplateApp::new(cc)))
+    );
 }
